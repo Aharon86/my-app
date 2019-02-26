0487ec56f0dc6515a99249d51f984bda313d34ad
Aharon Harutyunyan <Aharon.Harutyunyan@istc.com>
2019-02-26 21:06:31 +0400

    change

---
+++ src/components/header/index.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import Li from './returnLi';
+
+class Header extends Component {
+    showLi = () => {
+        let arr = ['Hello', 'Blog', 'About', 'Contacts'];
+        let arr2 = [];
+        for (let i = 0; i < arr.length; i++) {
+            arr2.push(<Li name ={arr[i]} />)
+            
+        }
+        return arr2;
+    }
+    render() {
+        return (
+            <header>
+                <ul>
+                    {this.showLi()}
+                    {/* <Li name ="Hello" text="we are happy"/>
+                    <Li name ="Blog"/>
+                    <Li name ="About"/>
+                    <Li name ="Contacts"/> */}
+                </ul>
+            </header>
+        );
+    }
+}
+
+export default Header;
